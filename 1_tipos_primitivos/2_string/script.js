5879246494e84b71e95c97cb800c8857176ca8c4
Veronica Modesto Machado <126088541+veronicamachado@users.noreply.github.com>
2024-03-19 22:11:33 -0300

    Add files via upload

---
+++ 1_tipos_primitivos/2_string/script.js
@@ -0,0 +1,16 @@
+const texto1 = "Kaike cupin";
+const texto2 = 'gabziin.rs';
+
+const texto3 = ' foi no "Banheiro"';
+
+console.log(texto1);
+console.log(texto2);
+console.log(texto3);
+
+console.log(texto2 + texto3);
+
+const cifrao = '\u0024';
+const aMaiusculo ='\u0041';
+
+console.log(cifrao);
+console.log(aMaiusculo);
---
+++ 1_tipos_primitivos/3_boolean/script.js
@@ -0,0 +1,19 @@
+const text1 = "Olá mundo";
+const text2 = 'Olá mundo';
+const senha = "senhasupersegura";
+const stringDeNumeros = "3642";
+
+//concatenacao (+)
+
+const citacao = "Meu nome é ";
+const meuNome = "Verônica "
+console.log(citacao + meuNome);
+
+//comparacao
+
+console.log(text1 === text2);
+
+const n1 = 1;
+const n2 = 2;
+
+console.log(n1 === n2);
