3e14d4516c8f2e5d2ecb3cb0d8402cd1d87a83a5
Veronica Modesto Machado <126088541+veronicamachado@users.noreply.github.com>
2024-03-25 20:19:28 -0300

    Add files via upload

---
+++ 2_declarando_variaveis/1_var_let_const/script.js
@@ -0,0 +1,37 @@
+/*var primeiro tipo de
+variavel funciona em qualquer lugar */
+var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+
+console.log(area);
+var area;
+
+/*let só permit que a 
+variavel seja utilizada
+após sua declaração*/
+
+let forma = 'retângulo';
+let altura2 = 5;
+let comprimento2 = 7;
+
+if(forma === 'retângulo'){
+    area = altura2 * comprimento2;
+}else{
+    area = (altura2 * comprimento2)/2;
+}
+console.log(area)
+// let area;
+
+/* A const é uma constante, ou seja, uma vez
+que a colocamos no programa não conseguimos
+alterá-la. */
+
+const forma3 = 'quadrado'
+const altura3 = 5;
+const comprimento3 = 7;
+
+const area3 = altura3 * comprimento3;
+
+console.log(area3);
---
+++ introJS3-24-mendes-main/1_tipos_primitivos/1_number/script.js
@@ -0,0 +1,17 @@
+//tipo number
+
+const meuNumero = 18;
+
+const telefone = 4499877-9661;
+
+const idade = 16;
+
+
+console.log(meuNumero);
+console.log(telefone);
+console.log(idade);
+
+
+const soma = meuNumero + idade;
+
+console.log(soma);
